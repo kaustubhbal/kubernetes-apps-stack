ed8744dd4728510932d6d58f0df72e8b594b638b
sachet94 <42930358+sachet94@users.noreply.github.com>
2021-12-23 09:00:15 +0530

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/weaveworks/eksctl/releases/download/v0.68.0/eksctl_Linux_amd64.tar.gz #eksctl version 0.67
+wget https://github.com/weaveworks/eksctl/releases/download/v0.68.0/eksctl_Linux_amd64.tar.gz
 tar -zxvf eksctl_Linux_amd64.tar.gz
 mv /tmp/eksctl /bin
 curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
